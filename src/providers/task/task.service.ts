698a0c3ac8bfdac9fd80ae7b787f34ea8640647e
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2017-09-26 15:24:06 -0300

    adicionando o metodo getAll => TaskService

--- src/providers/task/task.service.ts
+++ src/providers/task/task.service.ts
@@ -1,13 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 
+import { Task } from './../../models/task.model';
+
 @Injectable()
 export class TaskService {
 
   constructor(
     public storage: Storage
-  ) {}
+  ) { }
 
+  getAll(): Promise<Task[]> {
 
+    return fn();
+
+  }
 
 }
+
+function fn() {
+  return this.storage.ready()
+    .then((localForage: LocalForage) => {
+      let tasks: Task[] = [];
+      return this.storage.forEach((task: Task, key: string, iterationNumber: number) => {
+        if (key.indexOf('tasks.') > -1) {
+          tasks.push(task);
+        }
+      }).then(() => tasks);
+    }).catch(err => console.log("Erro ao abrir o storage: ", err));
+}
+
