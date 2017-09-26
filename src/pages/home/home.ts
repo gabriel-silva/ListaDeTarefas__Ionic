45eeb2d2615ae356c1c47c32aad61b832fd88f15
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2017-09-26 17:32:37 -0300

    adicionando método showAlert

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, LoadingController, Loading } from 'ionic-angular';
+import { NavController, LoadingController, Loading, AlertOptions, AlertController } from 'ionic-angular';
 
 import { TaskService } from './../../providers/task/task.service';
 import { Task } from './../../models/task.model';
@@ -9,20 +9,82 @@ import { Task } from './../../models/task.model';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  
+
   tasks: Task[] = [];
 
   constructor(
+    public alertCtrl: AlertController,
     public loadingCtrl: LoadingController,
     public navCtrl: NavController,
     public taskService: TaskService
-  ) {}
+  ) { }
 
-  ionViewDidLoad(){
+  ionViewDidLoad() {
     this.taskService.getAll()
-    .then((tasks: Task[]) => {      
-      this.tasks = tasks;
-    });
+      .then((tasks: Task[]) => {
+        this.tasks = tasks;
+      });
+  }
+
+  // os atributos do options é obrigatório
+  //menos o task
+  private showAlert(options: { itemSliding, title: string, type: string }, task?: Task): void {
+
+    let alertOptions: AlertOptions = {
+      title: options.title,
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Task title'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Save',
+          // função de callback
+          handler: (data) => {
+            let loading: Loading = this.showLoading(`Saving ${data.title} task...`);
+            let contextTask: Task;
+
+            switch (options.type) {
+              case 'create':
+                contextTask = new Task(data.title);
+                break;
+              case 'update':
+                task.title = data.title;
+                contextTask = task;
+                break;
+            }
+
+            this.taskService[options.type](contextTask)
+              .then((savedTask: Task) => {
+                if (options.type === 'create') {
+                  this.tasks.unshift(savedTask);
+                  loading.dismiss();
+                }
+                if (options.itemSliding) {
+                  options.itemSliding.close();
+                }
+
+              });
+          }
+
+        }
+
+      ]
+
+    };
+
+    if (options.type === 'update') {
+      alertOptions.inputs[0].value = task.title;
+    }
+
+    this.alertCtrl.create(alertOptions).present();
+
   }
 
   private showLoading(message?: string): Loading {
