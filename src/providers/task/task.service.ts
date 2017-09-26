b826531a02126ae5bc1dbff99364795d171fafaa
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2017-09-26 16:19:15 -0300

    construindo layout home

--- src/providers/task/task.service.ts
+++ src/providers/task/task.service.ts
@@ -11,36 +11,38 @@ export class TaskService {
   ) { }
 
   getAll(): Promise<Task[]> {
-    return fn();
+    
+    return this.storage.ready()
+      .then((localForage: LocalForage) => {
+
+        let tasks: Task[] = [];
+
+        return this.storage.forEach((task: Task, key: string, iterationNumber: number) => {
+          
+          if (key.indexOf('tasks.') > -1) {
+            tasks.push(task);
+          }
+
+        }).then(() => tasks);
+
+      });
+
   }
 
-  getById(id: number): Promise<Task>{
+  getById(id: number): Promise<Task> {
     return this.storage.get(`tasks.${id}`); //exemplo: task.45657687 
   }
 
-  create(task: Task): Promise<Task>{
+  create(task: Task): Promise<Task> {
     return this.storage.set(`tasks.${task.id}`, task);
   }
 
-  update(task: Task): Promise<Task>{
+  update(task: Task): Promise<Task> {
     return this.create(task);
   }
 
-  delete(id: number): Promise<boolean>{
+  delete(id: number): Promise<boolean> {
     return this.storage.remove(`tasks.${id}`).then(() => true);
   }
 
 }
-
-function fn() {
-  return this.storage.ready()
-    .then((localForage: LocalForage) => {
-      let tasks: Task[] = [];
-      return this.storage.forEach((task: Task, key: string, iterationNumber: number) => {
-        if (key.indexOf('tasks.') > -1) {
-          tasks.push(task);
-        }
-      }).then(() => tasks);
-    }).catch(err => console.log("Erro ao abrir o storage: ", err));
-}
-
